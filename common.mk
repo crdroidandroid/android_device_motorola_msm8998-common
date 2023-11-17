e0004dc09a027f741d79335749ad3a4aa87bb782
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-11-17 13:48:46 +0000

    Revert "fix build"
    This reverts commit eb9b92851ac3fc15794a665c83c0dfbdcb6d7541.

--- common.mk
+++ common.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    libbson.vendor \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0:64
 
